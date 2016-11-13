9ef72f2f25c5c406e933420d546380ca4d89c3d1
PhilipCanning <philiplfc@live.co.uk>
2016-11-13 22:03:09 +0000

    initial commit

---
+++ Question2/Question2/Question2.cpp
@@ -0,0 +1,16 @@
+// Question2.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <conio.h>
+#include <Windows.h>
+#include <string>
+#include <iomanip>
+
+
+int main()
+{
+    return 0;
+}
+
